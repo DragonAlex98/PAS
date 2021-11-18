44846a77c429a1933b90522f6ed3e18c62aad998
DragonAlex98 <37349526+DragonAlex98@users.noreply.github.com>
2021-11-18 17:08:33 +0100

    Update life2

--- life2.pm
+++ life2.pm
@@ -1,10 +1,40 @@
 /* Risultati CSV */
 /* time unit;media;varianza;dev std */
 const N = 3;            /* Initial number of cities */
+const warChance = 0.25;
 
 species P of [0, N];    /* people */
+species B of [0, N];    /* birth */
+species I of [0, N];    /* immigration */
+species D of [0, N];    /* death */
+species E of [0, N];    /* emigration */
+
+rule make_war_1 for i in [0,N] and j in [0,N] {
+    [!(i == j)] P[i]|P[j] -[ warChance ]-> P[i]|D[j]
+}
+
+rule make_war_2 for i in [0,N] and j in [0,N] {
+    [!(i == j)] P[i]|P[j] -[ warChance ]-> D[i]|P[j]
+}
+
+rule make_war_3 for i in [0,N] and j in [0,N] {
+    [!(i == j)] P[i]|P[j] -[ warChance ]-> P[i]|P[j]
+}
+
+rule make_war_4 for i in [0,N] and j in [0,N] {
+    [!(i == j)] P[i]|P[j] -[ warChance ]-> D[i]|D[j]
+}
+
+rule emigrate for i in [0,N] {
+    P[i] -[ %P[i] ]-> E[i]|P[(i+1)%N]|I[(i+1)%N]
+}
+
+rule reproduce for i in [0,N] {
+    P[i]|P[i] -[ %P[i] ]-> P[i]|P[i]|B[i]
+}
 
 param scale = 10;
+measure tot for i in [0,N] = #P[i];
 system balanced = P[i for i in [0, N]]<scale>;
 system unbalanced = P[i for i in [0, N-1]]<scale>|P[N-1]<3*scale>;
 
