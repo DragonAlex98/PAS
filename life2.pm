dd7bacb8da869119dba92f0d3001603077ccd46f
DragonAlex98 <37349526+DragonAlex98@users.noreply.github.com>
2021-11-21 23:13:22 +0100

    Update life2

--- life2.pm
+++ life2.pm
@@ -1,7 +1,7 @@
 /* Risultati CSV */
 /* time unit;media;varianza;dev std */
 const N = 3;            /* Initial number of cities */
-const warChance = 0.25;
+const warChance = 1/3;
 
 species P of [0, N];    /* people */
 species B of [0, N];    /* birth */
@@ -9,20 +9,17 @@ species I of [0, N];    /* immigration */
 species D of [0, N];    /* death */
 species E of [0, N];    /* emigration */
 
+/* TODO: %P is not correct, I need the the %P with respect to P[], not all */
 rule make_war_1 for i in [0,N] and j in [0,N] {
-    [!(i == j)] P[i]|P[j] -[ warChance ]-> P[i]|D[j]
+    [!(i == j)] P[i]|P[j] -[ warChance * %P[i] * %P[j] ]-> P[i]|D[j]
 }
 
 rule make_war_2 for i in [0,N] and j in [0,N] {
-    [!(i == j)] P[i]|P[j] -[ warChance ]-> D[i]|P[j]
+    [!(i == j)] P[i]|P[j] -[ warChance * %P[i] * %P[j] ]-> D[i]|P[j]
 }
 
 rule make_war_3 for i in [0,N] and j in [0,N] {
-    [!(i == j)] P[i]|P[j] -[ warChance ]-> P[i]|P[j]
-}
-
-rule make_war_4 for i in [0,N] and j in [0,N] {
-    [!(i == j)] P[i]|P[j] -[ warChance ]-> D[i]|D[j]
+    [!(i == j)] P[i]|P[j] -[ warChance * %P[i] * %P[j] ]-> D[i]|D[j]
 }
 
 rule emigrate for i in [0,N] {
@@ -34,7 +31,6 @@ rule reproduce for i in [0,N] {
 }
 
 param scale = 10;
-measure tot for i in [0,N] = #P[i];
 system balanced = P[i for i in [0, N]]<scale>;
 system unbalanced = P[i for i in [0, N-1]]<scale>|P[N-1]<3*scale>;
 
